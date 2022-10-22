6c45c6987d753951de351a794c48cb820f8cd5df
bulbanaut <aleks13102004@gmail.com>
2022-10-22 14:51:07 +0500

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let b = String::from("Мальчик");
+    let g = String::from("Девочка");
+
+    println!("Кто ты: Мальчик или Девочка?");
+    let input = read_var();
+    if input == b {
+        println!("Мне нравятся мальчики");
+    } else if input == g {
+        println!("Мне нравятся девочки");
+    } else {
+        println!("шо");
+    }
+}
+
+fn read_var() -> String {
+    loop {
+        let mut x =String::new();
+        stdin()
+            .read_line(&mut x)
+            .expect("Ошибка чтения ввода"); //считывание ввода и запись его в переменную X (с точки зрения компилятора это одна строка)
+        let x = x.trim().to_string();
+        return x;
+    }
 }
